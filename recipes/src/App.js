dcdf6b8f14f9e3b53b84b6d7e4c5ce81567ea0e9
Sho <svang14@msudenver.edu>
2024-05-21 14:21:30 -0600

    updatedDB

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -4,13 +4,14 @@ import SignUp from './comps/SignUp';
 import CreateRec from './comps/CreateRec';
 import ViewRec from './comps/ViewRec';
 import ViewUsers from './comps/ViewUsers';
-
+import HomePage from './comps/HomePage';
 function App() {
   return (
     <>
       {/* <Login/> */}
+      <HomePage/>
       {/* <SignUp/> */}
-      <CreateRec/>
+      {/* <CreateRec/> */}
       {/* <ViewRec/> */}
       {/* <ViewUsers/> */}
     </>
---
+++ recipes/src/comps/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome To Potluck Recipes</h1>
+        <div>
+          <button>Sign out</button>
+          <button>View Recipes</button>
+          <button>Create Recipe</button>
+        </div>
+      </div>
+    );
+  }
+  
+
+export default HomePage;
