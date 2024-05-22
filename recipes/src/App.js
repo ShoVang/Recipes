f49b3298543345d0297174bdc4176d65e6ca1921
Sho <svang14@msudenver.edu>
2024-05-22 17:34:25 -0600

    RoutWorks

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,20 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login  from './comps/Login';
+import Login from './comps/Login';
 import SignUp from './comps/SignUp';
 import CreateRec from './comps/CreateRec';
 import ViewRec from './comps/ViewRec';
 import ViewUsers from './comps/ViewUsers';
 import HomePage from './comps/HomePage';
+
 function App() {
   return (
-    <>
-      {/* <Login/> */}
-      {/* <HomePage/> */}
-      {/* <SignUp/> */}
-      {/* <CreateRec/> */}
-      <ViewRec/>
-      {/* <ViewUsers/> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/create-recipe" element={<CreateRec />} />
+        <Route path="/view-recipes" element={<ViewRec />} />
+        <Route path="/view-users" element={<ViewUsers />} />
+      </Routes>
+    </Router>
   );
 }
 
--- recipes/src/comps/HomePage.js
+++ recipes/src/comps/HomePage.js
@@ -1,18 +1,29 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
-    return (
+  return (
+    <div>
+      <h1>Welcome To Potluck Recipes</h1>
       <div>
-        <h1>Welcome To Potluck Recipes</h1>
-        <div>
-          <button>Sign out</button>
+        <Link to="/login">
+          <button>Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+        <Link to="/view-recipes">
           <button>View Recipes</button>
+        </Link>
+        <Link to="/create-recipe">
           <button>Create Recipe</button>
-        </div>
+        </Link>
+        <Link to="/view-users">
+          <button>View Users</button>
+        </Link>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
 
 export default HomePage;
