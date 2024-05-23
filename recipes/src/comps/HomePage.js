538d7100fdf46f66f24e3e93731ef892de7039d6
Sho <svang14@msudenver.edu>
2024-05-23 12:24:38 -0600

    Works

--- recipes/src/comps/HomePage.js
+++ recipes/src/comps/HomePage.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css'; // Assuming you have a separate CSS file for styling
 
 function HomePage() {
   return (
-    <div>
+    <div className="index-container">
       <h1>Welcome To Potluck Recipes</h1>
-      <div>
 
+      <div className="bio">
+        <p>
+        Explore a world of delicious recipes, share your culinary creations, 
+        and connect with fellow food enthusiasts on Potluck Recipes. 
+        Whether you're a seasoned chef or just starting your cooking journey, there's something here for everyone!
+        </p>
+      </div>
+      <div className="buttons">
         <Link to="/view-recipes">
-          <button>View Recipes</button>
+          <button className="button">View Recipes</button>
         </Link>
         <Link to="/create-recipe">
-          <button>Create Recipe</button>
+          <button className="button">Create Recipe</button>
         </Link>
-        <Link to="/view-users">
-          <button>View Users</button>
-        </Link>
+        {/* <Link to="/view-users">
+          <button className="button">View Users</button>
+        </Link> */}
       </div>
     </div>
   );
